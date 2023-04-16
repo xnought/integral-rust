50bd7081142fd796570f4c6634a1702d48f1f52e
xnought <bertuccd@oregonstate.edu>
2023-04-15 18:43:01 -0700

    feat: donezo

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# integral-rust
+# Integral Rust
+
+Approximate integration of a function from point a to b for a single variable x.
+
+For example,
+
+```rust
+use std::f64::consts::E;
+
+// not normalized because I want to annoy stats people
+fn bell_curve(x: f64) -> f64 {
+    // e^{-x^2}
+    E.powf(-x.powi(2))
+}
+
+fn main() {
+	// setup for
+	// \int_{0}^{2} e^{-x^2} dx
+	let a = 0.0;
+	let b = 2.0;
+	let num_iterations = 100_000;
+    let area = integrate(bell_curve, a, b, 100_000);
+
+    println!("{}", area);
+    // >> 0.8820915739164501
+    // in actuality, it is supposed to be 0.882081390762. Pretty darn good.
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::f64::consts::E;
+
+/**
+ * returns an area under the curve from a to b
+ *
+ * a must be larger than b and define the number of rectangles to approximate with
+ *
+ * TODO: parallelize this
+ * TODO:
+ */
+fn integrate<F>(f: F, a: f64, b: f64, num_rectangles: usize) -> f64
+where
+    F: Fn(f64) -> f64,
+{
+    let dx = (b - a) / num_rectangles as f64;
+    let mut x = a;
+    let mut area_a_to_b = 0.0;
+
+    while x <= b {
+        let slice_height = f(x);
+        let slice_area = slice_height * dx;
+
+        // accumulate area
+        area_a_to_b += slice_area;
+        // move along
+        x += dx;
+    }
+
+    return area_a_to_b;
+}
+
+// not normalized because I want to annoy stats people
+fn bell_curve(x: f64) -> f64 {
+    // e^{-x^2}
+    E.powf(-x.powi(2))
+}
+
+fn main() {
+    let area = integrate(bell_curve, 0.0, 2.0, 100_000);
+
+    println!("{}", area);
+    // \int_{0}^{2} e^{-x^2} dx = 0.8820915739164501
+    // in actuality, it is supposed to be 0.882081390762, so pretty good!
+}
