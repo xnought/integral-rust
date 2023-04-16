560401f9e91c102e25365529b91c408a70774b8a
xnought <bertuccd@oregonstate.edu>
2023-04-15 20:17:17 -0700

    feat: monte carlo integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::{thread_rng, Rng};
+
+/**
+ * integration by simulation and finding the area that way
+ */
+fn monte_carlo_integrate<F>(f: F, a: f64, b: f64, y_min: f64, y_max: f64, num_random: usize) -> f64
+where
+    F: Fn(f64) -> f64,
+{
+    let mut rng = thread_rng();
+    let total_area = (b - a) * (y_max - y_min); //width*height
+
+    let mut num_inside = 0;
+    // generate random numbers of a box defined by a->b|a->height
+    for _ in 0..num_random {
+        let random_x = rng.gen_range(a..b);
+        let random_y = rng.gen_range(y_min..y_max);
+
+        // point lines outside the area if y larger than the output y
+        let f_y = f(random_x);
+        let inside = random_y <= f_y;
+        if inside {
+            num_inside += 1;
+            // println!("{}, {}", random_x, random_y);
+            // println!("inside");
+        } else {
+            // println!("outside");
+        }
+    }
+    let ratio = num_inside as f64 / num_random as f64;
+    total_area * ratio
+}
+
 /**
  * returns an area under the curve from a to b
  *
@@ -32,8 +65,10 @@ fn main() {
     // e^{-x^2}
     let bell_curve = |x: f64| (-x.powi(2)).exp();
     let area = integrate(bell_curve, 0.0, 2.0, 100_000);
+    let area2 = monte_carlo_integrate(bell_curve, 0.0, 2.0, 0.0, 1.0, 100_000);
 
     println!("{}", area);
     // \int_{0}^{2} e^{-x^2} dx = 0.8820915739164501
     // supposed to be 0.882081390762, so pretty good!
+    println!("{}", area2);
 }
